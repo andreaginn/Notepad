0ef4c5e52c26c2bc78ea44d257ce1bcd9157568e
Rose Ginn <andrearginn@gmail.com>
2023-04-26 14:36:18 -0700

    deploying

--- README.md
+++
@@ -1,9 +0,0 @@
-# Notepad
-
-## Description
-
-An app using JS, HTML, and CSS that allows the user to add and save multiple notes, each including a title and description, in local storage. 
-
-## Installation
-
-App deployed via Heroku: https://the-notepad.herokuapp.com/
--- server.js
+++ server.js
@@ -4,42 +4,43 @@ const errorHandler = require('npm2/lib/utils/error-handler');
 const path = require('path');
 const util = require('util');
 const { v4: uuidv4 } = require('uuid');
+const notes = require("./db/db.json")
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static('public'));
 
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'index.html'));
+    res.sendFile(path.join(__dirname, '/public/index.html'));
 });
 
 app.get('/notes', (req, res) => {
-    res.sendFile(path.join(__dirname, 'notes.html'));
+    res.sendFile(path.join(__dirname, '/public/notes.html'));
 });
 
-app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, 'index.html'));
-});
 
-app.get('api/notes', (req, res) => {
-    fs.readFile(path.join(__dirname, 'db.json'), 'utf8', (err, data) => {
-        if (err) throw err;
-        res.json(JSON.parse(data));
-    });
+app.get('/api/notes', (req, res) => {
+    res.json(notes)
 });
 
 app.post('/api/notes', (req, res) => {
-    fs.readFile(path.join(__dirname, 'db.json'), 'utf8', (err, data) => {
-        if (err) throw err;
-        const notes = JSON.parse(data);
-        const newNote = req.body;
-        newNote.id = uuidv4();
-        notes.push(newNote);
-        fs.writeFile(path.join(__dirname, 'db.json'), JSON.stringify(notes), err => {
-            if (err) throw err;
-            res.json(newNote);
-        });
-    });
+    // fs.readFile(path.join(__dirname, 'db.json'), 'utf8', (err, data) => {
+    //     if (err) throw err;
+    //     const notes = JSON.parse(data);
+    //     const newNote = req.body;
+    //     newNote.id = uuidv4();
+    //     notes.push(newNote);
+    //     fs.writeFile(path.join(__dirname, 'db.json'), JSON.stringify(notes), err => {
+    //         if (err) throw err;
+    //         res.json(newNote);
+    //     });
+    // });
+    notes.push(req.body)
+    fs.writeFileSync('./db/db.json', JSON.stringify(notes))
+    res.json(notes)
 });
 
 
